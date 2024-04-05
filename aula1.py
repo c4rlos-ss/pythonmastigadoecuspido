020a8f7624931c00af193e7d81f7ad907c05e800
carlos Eduardo <73598418+c4rlos-ss@users.noreply.github.com>
2024-04-05 19:13:54 +0000

    tipos em python

---
+++ aula1.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("clear")
+
+nome = input("qual seu nome: ")
+idade = input("qual sua idade: ")
+peso = input("qual seu pesso: ")
+
+print(f"\nnome: {nome}")
+print(f"a idade digitada: {idade}")
+print(f"peso: {peso:.3f}")
