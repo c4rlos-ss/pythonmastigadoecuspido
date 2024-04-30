229d88a7225e280d24ff51f99846a7e123d3fc8a
carlos Eduardo <73598418+c4rlos-ss@users.noreply.github.com>
2024-04-30 19:54:57 +0000

    hjhjhj

---
+++ atividade.py
@@ -0,0 +1,3 @@
+numero = float(input("diite o valor em metros: "))
+
+print(f"o valor em centimetros foi {numero:.1f}00 cm")
--- atividade16_funçao.py
+++ atividade16_funçao.py
@@ -1,8 +1,4 @@
 
-n1 = 0
-n2 = 0
-operador = "0"
-
 def calcular(operador, n1, n2):
     if operador == "+":
         return n1 + n2
@@ -13,13 +9,13 @@ def calcular(operador, n1, n2):
     elif operador == "*":
         return n1 * n2
     else:
-        print("operador invalido, tente novamente: ")
-        return
+        return print("error")
 
 n1 = float(input("digite o primeiro numero: "))
 n2 = float(input("digite o segundo numero: "))
 
 operador = input("deseja utilizar qual operador aritimetico: ")
+ 
+resultado = calcular(operador, n1, n2)
 
-calcular(operador, n1, n2)
-print(f"o valor da soma foi {(n1 + n2) / 2}")
+print(resultado)
